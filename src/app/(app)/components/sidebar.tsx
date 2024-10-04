0435f6dfe5b92202bfd1c8556374a52b011cd690
Nathan Palatin <nath.palatin@gmail.com>
2024-10-04 02:49:33 -0300

    chore: finalizado primeira parte do figma.

--- src/app/(app)/components/sidebar.tsx
+++ src/app/(app)/components/sidebar.tsx
@@ -17,7 +17,7 @@ export function SidebarToggle() {
 			<div
 				className={`${isSidebarOpen ? 'w-64' : 'w-0'} transition-all duration-300 bg-zinc-200 overflow-hidden`}
 			>
-				<div className="flex flex-col mt-10 min-h-screen p-4 text-white">
+				<div className="flex flex-col mt-10 h-screen p-4 text-white">
 					<Image
 						alt="logo sidebar"
 						width={140}
@@ -25,16 +25,73 @@ export function SidebarToggle() {
 						height={70}
 						src={require('@/assets/logo-sidebar.png')}
 					/>
-					<nav>
+					<nav className="mt-8 px-2">
 						<ul>
-							<li>
-								<a href="#">Inicial</a>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Inicial
+								</a>
 							</li>
-							<li>
-								<a href="#">Análises e recomendações</a>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Análises e recomendações
+								</a>
 							</li>
-							<li>
-								<a href="#">Minhas finanças</a>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Minhas finanças
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Carteiras
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Planejamento
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Investimento
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Crédito
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Seguros
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Cartão
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Store
+								</a>
+							</li>
+							<li className="flex items-center gap-2">
+								<div className="w-2 h-2 bg-zinc-500" />
+								<a href="#" className="text-muted-foreground text-sm">
+									Ferramentas
+								</a>
 							</li>
 						</ul>
 					</nav>
@@ -43,9 +100,9 @@ export function SidebarToggle() {
 
 			<button
 				onClick={toggleSidebar}
-				className={`fixed top-6 ${
+				className={`absolute top-6 ${
 					isSidebarOpen ? 'left-60 rotate-180' : '-left-4'
-				} z-50 p-2 bg-muted-foreground transition-all duration-300 text-white rounded-full`}
+				} z-50 p-1 bg-muted-foreground transition-all duration-300 text-white rounded-full`}
 			>
 				<ChevronRight />
 			</button>
